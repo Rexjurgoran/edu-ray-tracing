b465f2809bf9ec1788b2bbed68097cc50d3fa975
Henßler <marvin.henssler@harting.com>
2025-02-07 16:00:42 +0100

    Start implementing constructor for bvh

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,9 +1,36 @@
-use crate::{aabb::Aabb, interval::{interval, Interval}, ray::Ray, sphere::Hittable};
+use crate::{aabb::{aabb_from_aabb, Aabb}, interval::{interval, Interval}, ray::Ray, sphere::Hittable};
 
 pub struct BvhNode {
-    left: Box<dyn Hittable>,
-    right: Box<dyn Hittable>,
-    bbox: Aabb
+    left: usize,
+    right: usize,
+    bbox: Aabb,
+}
+
+static NODES:Vec<BvhNode> = Vec::new();
+
+pub fn bvh_node(objects: &Vec<Box<dyn Hittable>>, nodes: &mut Vec<BvhNode>, start: usize, end: usize) -> usize {
+    let object_span = end - start;
+    let mut left = start ;
+    let mut right = start;
+
+    if object_span == 1 {
+        // Keep initial values for left and right
+    } else if object_span == 2 {
+        right = start + 1;
+    } else {
+
+        // TODO: sorting of objects by random axis
+
+        let mid = start + object_span / 2;
+
+        left = bvh_node(objects, nodes, start, mid);
+        right = bvh_node(objects, nodes, mid, end);
+    }
+
+    let bbox = aabb_from_aabb(nodes[left].bounding_box(), nodes[right].bounding_box());
+    let node = BvhNode { left, right, bbox };
+    nodes.push(node);
+    nodes.len()
 }
 
 impl Hittable for BvhNode {
@@ -12,8 +39,8 @@ impl Hittable for BvhNode {
             return false;
         }
 
-        let hit_left = self.left.hit(r, ray_t.clone(), rec);
-        let hit_right = self.right.hit(
+        let hit_left = NODES[self.left].hit(r, ray_t.clone(), rec);
+        let hit_right = NODES[self.right].hit(
             r, 
             interval(ray_t.min, if hit_left { rec.t } else { ray_t.max }), 
             rec
