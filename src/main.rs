7400490b9b371a8c704ce4efcfe1652f05dff7cc
Rexjurgoran <hensslermarvin@gmail.com>
2025-02-27 19:47:52 +0100

    <4.3> Rendering The Solid Checker Texture

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::rc::Rc;
+use std::{i32, rc::Rc};
 
 use bvh::BvhNode;
 use camera::Camera;
-use color::color;
+use color::{color, Color};
 use hittable_list::HittableList;
 use material::Material;
 use rtweekend::{random_double, random_double_from};
 use sphere::Sphere;
-use texture::CheckerTexture;
-use vec3::{random, random_from, vec3};
+use texture::{CheckerTexture, Texture};
+use vec3::{random, random_from, vec3, Vec3};
 
 mod aabb;
 mod bvh;
@@ -23,7 +23,7 @@ mod sphere;
 mod texture;
 mod vec3;
 
-fn main() {
+fn bouncing_spheres() {
     let mut world = HittableList::default();
 
     let checker = Rc::new(CheckerTexture::from_colors(
@@ -102,4 +102,47 @@ fn main() {
     cam.focus_dist = 10.0;
 
     cam.render(&world);
+}
+
+fn checkered_spheres() {
+    let mut world = HittableList::default();
+
+    let checker = Rc::new(CheckerTexture::from_colors(
+        0.32,
+        color(0.2, 0.3, 0.1),
+        color(0.9, 0.9, 0.9),
+    ));
+    world.add(Rc::new(Sphere::new(
+        vec3(0.0, -10.0, 0.0),
+        10.0,
+        Material::lambertian_from_tex(checker.clone()),
+    )));
+    world.add(Rc::new(Sphere::new(
+        vec3(0.0, 10.0, 0.0),
+        10.0,
+        Material::lambertian_from_tex(checker),
+    )));
+
+    let mut cam: Camera = Default::default();
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20.0;
+    cam.lookfrom = vec3(13.0, 2.0, 3.0);
+    cam.lookat = vec3(0.0, 0.0, 0.0);
+    cam.vup = vec3(0.0, 1.0, 0.0);
+
+    cam.defocus_angle = 0.0;
+
+    cam.render(&world);
+}
+
+fn main() {
+    match 2 {
+        1 => bouncing_spheres(),
+        2 => checkered_spheres(),
+        i32::MIN..=i32::MAX => !panic!(),
+    }
 }
