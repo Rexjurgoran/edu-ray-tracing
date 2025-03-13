88ee351dfe4632abf0fc7b8259f41d273a570dc4
Marvin Henßler <marvinhenssler@aol.com>
2025-03-13 10:29:37 +0100

    <5.2> Smoothing out the Result

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,6 +1,9 @@
 use rand::seq::index;
 
-use crate::{rtweekend::{random_double, random_int_from}, vec3::Vec3};
+use crate::{
+    rtweekend::{random_double, random_int_from},
+    vec3::Vec3,
+};
 
 pub struct Perlin {
     randfloat: Vec<f64>,
@@ -32,11 +35,28 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Vec3) -> f64 {
-        let i = ((4.0 * p.x) as i32 & 255) as usize;
-        let j = ((4.0 * p.y) as i32 & 255) as usize;
-        let k = ((4.0 * p.z) as i32 & 255) as usize;
+        let u = p.x - f64::floor(p.x);
+        let v = p.y - f64::floor(p.y);
+        let w = p.z - f64::floor(p.z);
 
-        self.randfloat[(self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize]
+        let i = f64::floor(p.x) as usize;
+        let j = f64::floor(p.y) as usize;
+        let k = f64::floor(p.z) as usize;
+
+        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[(self.perm_x[(i + di) & 255]
+                        ^ self.perm_y[(j + dj) & 255]
+                        ^ self.perm_z[(k + dk) & 255])
+                        as usize]
+                }
+            }
+        }
+
+        Perlin::trilinear_interp(c, u, v, w)
     }
 
     fn generate_perm(point_count: i32, p: &mut Vec<i32>) {
@@ -53,5 +73,21 @@ impl Perlin {
             p[i as usize] = p[target as usize];
             p[target as usize] = tmp;
         }
+    }
+
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
+                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
+                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
